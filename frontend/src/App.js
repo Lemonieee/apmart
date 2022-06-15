f655039c9986aa55ebeae98e426541cc9fbc02d1
Shanie <shanie001024@gmail.com>
2022-06-15 23:27:26 +0800

    S5-ReactBootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,34 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Product from './pages/Product';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">APMart</Link>
+          <Navbar bg="primary" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>APMart</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<Product />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<Product />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
